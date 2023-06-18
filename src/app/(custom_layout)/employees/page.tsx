8720a34090f07a666442fcc583788791b34fc300
Jakub Madyda <jakub.madyda17@gmail.com>
2023-06-18 12:10:28 +0200

    employees cards with fake images

--- src/app/(custom_layout)/employees/page.tsx
+++ src/app/(custom_layout)/employees/page.tsx
@@ -1,6 +1,46 @@
+'use client'
+
+import {useEffect, useState} from "react";
+import {apiCall} from "@/api/api";
+import Card from "@/components/Card";
+
+interface Employee {
+    fullName: string;
+    seniority: number;
+    photo: string;
+    position: string;
+}
+
+interface EmployeeId extends Employee {
+    id: number;
+}
+
 function Page() {
+    const [employees, setEmployees] = useState<EmployeeId[]>([])
+
+    useEffect(() => {
+        const controller = new AbortController();
+
+        apiCall<EmployeeId[]>({endpoint: "employees", signal: controller.signal})
+            .then(setEmployees)
+            .catch(console.error);
+
+        return () => {
+            controller.abort();
+        }
+
+    }, [])
+
     return (
-        <div>employees</div>
+        <div className="flex gap-4 flex-wrap justify-center max-w-5xl mx-auto">
+            {employees.map(({fullName, seniority, id, position, photo}) => (
+                <Card
+                    fullName={fullName}
+                    position={position}
+                    photo={photo}
+                    seniority={seniority}
+                    key={id} />
+                ))}</div>
     );
 }
 
---
+++ src/components/Card.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import {faker} from "@faker-js/faker";
+
+type CardProps = {
+    fullName: string;
+    position: string;
+    photo: string;
+    seniority: number;
+}
+
+const Card = ({ fullName, position, photo, seniority }: CardProps) => {
+    return (
+        <div className="bg-white shadow-md rounded-lg p-6">
+            {!photo && (
+                <Image src={faker.image.urlLoremFlickr({category: 'people'})} alt={fullName} width="100" height="120" className="w-full mb-4 rounded-md" />
+            )}
+            <h2 className="text-xl text-gray-600 font-bold mb-2">{fullName}</h2>
+            <p className="text-gray-600 mb-2">{position}</p>
+            <p className="text-gray-600">
+                Seniority: {seniority} {seniority === 1 ? 'year' : 'years'}
+            </p>
+        </div>
+    );
+};
+
+export default Card;
