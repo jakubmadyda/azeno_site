cf8966e91e897da6382deac2f716b462923609ef
Jakub Madyda <jakub.madyda17@gmail.com>
2023-06-24 11:12:12 +0200

    sticky navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,14 @@
-import Link from "next/link";
 import Image from "next/image";
 
 function Navbar() {
     return (
         <nav
-            className="flex justify-center-items gap-10 group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
-            <Link href="/"><Image alt="Azeno Company logo" src="/logo.png" width="50" height="63"/></Link>
-            <Link href="/">Home</Link>
-            <Link href="/contact">Contact</Link>
-            <Link href="/about">About us</Link>
-            <Link href="/employees">Employees</Link>
+            className="sticky top-0 z-50 flex justify-center-items gap-10 group bg-gray-800 bg-opacity-70 rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+            <a href="#home"><Image alt="Azeno Company logo" src="/logo.png" width="50" height="63"/></a>
+            <a href="#home">Home</a>
+            <a href="#about">About us</a>
+            <a href="#employees">Employees</a>
+            <a href="#form">Contact</a>
         </nav>
     );
 }
