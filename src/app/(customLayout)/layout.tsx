32d8b3ff87f53388c00016739f6ceb21b4f6f948
Jakub Madyda <jakub.madyda17@gmail.com>
2023-06-24 12:52:54 +0200

    testing version

---
+++ src/app/(customLayout)/layout.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import {notFound} from "next/navigation";
+import {NextIntlClientProvider} from "next-intl";
+
+export function generateStaticParams() {
+    return [{locale: 'en'}, {locale: 'pl'}];
+}
+
+async function Layout({children, params: {locale}}: { children: React.ReactNode }) {
+    let messages;
+    console.log(locale)
+    try {
+        // messages = (await import(`../../messages/${locale}.json`)).default;
+    } catch (error) {
+        // notFound();
+    }
+
+    return (
+        <section>
+                <Navbar/>
+                {children}
+                <Footer/>
+        </section>
+    )
+
+}
+
+export default Layout;
--- src/components/FormContact.tsx
+++ src/components/FormContact.tsx
@@ -2,7 +2,7 @@
 
 import {SubmitHandler, useForm} from "react-hook-form";
 import React, {useEffect, useState} from "react";
-import {apiCall} from "@/api/api";
+import {apiCall} from "@/helpers/helpers";
 
 type Inputs = {
     fullName: string,
